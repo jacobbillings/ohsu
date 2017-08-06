d8b07ed0d01836f407a08b88694be606652229e0
Jacob Billings <jacob.m.billings@gmail.com>
2017-08-06 16:05:17 -0700

    add handling for offset param higher than # rows; general handling for no rows read

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <?php 
+/*
+Reads rows from either person.csv or items.csv(which is determined by URL parameter request), located in same directory as this file.
 
-///set defaults
+URL parameters offset and limit determine how many rows to skip and how many rows to read, respectively. Defaults are request=person&offset=0&limit=1.
+
+Reads only to the end of the csv file if limit parameter is higher than remaining number of rows.
+
+Returns empty object ("{}") if offset parameter is higher than the number of rows.
+
+Returns to the browser a JSON-encoded string. Top-level key is "person" or "items".
+
+Each element below that is a key-value pair based on an individual row in the requested csv. The key is the idx number given as the first field in the csv row. The value is an array of key-value pairs where a given key is the name of a csv field, e.g. "motionOnly" and the value is the value for that field in the csv row corresponding to the idx number. 
+
+Example: request=person&offset=0&limit=2 would read the first two rows of person.csv and give the following JSON-encoded string(reformatted for better readability here):
+
+{"person":{
+	"256":{"gender":"0","motionOnly":"0","status":"1"},
+	"257":{"gender":"0","motionOnly":"1","status":"1"}
+	}
+}
+*/
+
+///set parameter defaults
 $request = "person";
 $offset = 0;
 $limit = 1;
@@ -50,6 +71,11 @@ function csv_to_json($request, $offset, $limit) {
 	while ($row <= $limit) {
 		$row_array = fgetcsv($handle);
 
+		//Stop process if we reach the end of the csv file
+		if ($row_array == NULL) {
+			break;
+		}
+
 		/*
 		For each row, we want to create a key-value pair,
 		with the idx field($row_array[0]) as the key and an array as the value.
@@ -65,6 +91,12 @@ function csv_to_json($request, $offset, $limit) {
 		$row++;
 	}
 
+	/*
+	JSON encode and return the array we've built, adding a top level element named after the request type. return "{}" if the offset parameter put us past all the rows.
+	*/
+	if ($json_array == NULL) {
+		return "{}";
+	}
 	return json_encode(array($request => $json_array));
 }
 
